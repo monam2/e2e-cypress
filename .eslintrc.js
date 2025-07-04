f4f8fc7441b3d6a8cbcd911a8b42a46983b41377
monam2 <kangcw0107@gmail.com>
2025-07-05 00:12:41 +0900

    lint, prettier 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,21 @@
 module.exports = {
-  extends: ["next/core-web-vitals", "plugin:prettier/recommended"],
-  plugins: ["prettier"],
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'prettier', // Prettier와 충돌하는 ESLint 규칙 비활성화
+  ],
+  plugins: ['prettier', 'cypress'],
+  rules: {
+    'prettier/prettier': 'error', // Prettier 규칙을 ESLint 에러로 표시
+    'no-undef': 'error',
+  },
+  // Cypress 파일용 설정
   overrides: [
     {
-      files: ["cypress/**/*.js"],
-      extends: ["plugin:cypress/recommended"],
+      files: ['cypress/**/*.js'],
+      extends: ['plugin:cypress/recommended'],
       env: {
-        "cypress/globals": true,
+        'cypress/globals': true,
       },
     },
   ],
